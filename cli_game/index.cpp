f837ffbad8361b369504b7fddd004d92d4f39dac
AbijuruT <abijuruyves@gmail.com>
2022-01-22 11:58:14 +0200

    Initial commit

---
+++ cli_game/index.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file index.cpp
+ * @author  Abijuru
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-22
+ * 
+ * @copyright Copyright (c) 2022
+ *
+ * 
+ */
+#include <iostream>
+using namespace std;
+void welcome_msg(){
+    cout << "Welcome to this game" << endl;
+}
+
+int main(){
+    welcome_msg();
+}
